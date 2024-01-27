c8c665f05c11f0fd2477f91b200fc09c57732961
liuzibo <179499725@qq.com>
2024-01-27 19:45:53 +0800

    P1205

--- LuoGu/入门4_数组/P1161_开灯.cpp
+++ LuoGu/入门4_数组/P1161_开灯.cpp
@@ -1,13 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool light[2000005];
+
 int main()
 {
-    int n;
+    int n, temp, t;
+    double a;
     cin >> n;
-    while (n--)
+    for (int i = 0; i < n; i++)
     {
-        
+        cin >> a >> t;
+        for (int j = 1; j <= t; j++)
+        {
+            temp = floor(a * j);
+            if (light[temp] == false)
+            {
+                light[temp] = true;
+            }
+            else
+            {
+                light[temp] = false;
+            }
+        }
+    }
+    for (int i = 0; i < 2000005; i++)
+    {
+        if (light[i] == true)
+        {
+            cout << i;
+        }
     }
     return 0;
 }
--- LuoGu/入门4_数组/P1205_方块转换.cpp
+++ LuoGu/入门4_数组/P1205_方块转换.cpp
@@ -1,75 +1,96 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-char square[15][15];
-char tempSquare[15][15];
-char resSquare[15][15];
 int n;
 
-// void copy()
-// {
-//     for (int i = 0; i < n; i++)
-//     {
-//         for (int j = 0; j < n; j++)
-//         {
-//             square[i][j] = tempSquare[i][j];
-//         }
-//     }
-// }
-
-// void rotate()
-// {
-//     for (int i = 0; i < n; i++)
-//     {
-//         for (int j = 0; j < n; j++)
-//         {
-//             tempSquare[j][n - i - 1] = square[i][j];
-//         }
-//     }
-//     copy();
-// }
-
-// bool compare()
-// {
-//     for (int i = 0; i < n; i++)
-//     {
-//         for (int j = 0; j < n; j++)
-//         {
-//             if (square[i][j] != resSquare[i][j])
-//             {
-//                 return false;
-//             }
-//         }
-//     }
-//     return true;
-// }
+struct Matrix
+{
+    char square[15][15];
+    void init()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cin >> square[i][j];
+            }
+        }
+    }
+    Matrix rotate()
+    {
+        Matrix res;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                res.square[i][j] = square[n - j - 1][i];
+            }
+        }
+        return res;
+    }
+    Matrix reflex()
+    {
+        Matrix res;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                res.square[i][j] = square[i][n - j - 1];
+            }
+        }
+        return res;
+    }
+    bool operator==(Matrix b)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (square[i][j] != b.square[i][j])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+} from, to, mid;
 
 int main()
 {
     cin >> n;
-    for (int i = 0; i < n; i++)
+    from.init();
+    to.init();
+    mid = from;
+    for (int i = 1; i < 4; i++)
     {
-        for (int j = 0; j < n; j++)
+        mid = mid.rotate();
+        if (mid == to)
         {
-            cin >> square[i][j];
+            cout << i << endl;
+            return 0;
         }
     }
-    for (int i = 0; i < n; i++)
+    mid = from.reflex();
+    if (mid == to)
     {
-        for (int j = 0; j < n; j++)
+        cout << 4 << endl;
+        return 0;
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        mid = mid.rotate();
+        if (mid == to)
         {
-            cin >> resSquare[i][j];
+            cout << 5 << endl;
+            return 0;
         }
     }
-    rotate();
-    for (int i = 0; i < n; i++)
+    if (from == to)
     {
-        for (int j = 0; j < n; j++)
-        {
-            cout << square[i][j];
-        }
-        cout << endl;
+        cout << 6 << endl;
+        return 0;
     }
-    cout << compare() << endl;
+    cout << 7 << endl;
     return 0;
 }
