380cef8986b753cac67b8f96e461bed12f04a82c
liuzibo <179499725@qq.com>
2024-03-09 20:39:23 +0800

    test

--- LuoGu/入门5_字符串/P3741_honoka的键盘.cpp
+++ LuoGu/入门5_字符串/P3741_honoka的键盘.cpp
@@ -3,6 +3,24 @@ using namespace std;
 
 int main()
 {
-    
+    int len;
+    char s[105];
+    cin >> len;
+    cin >> s + 1;
+    int cnt = 0;
+    int flag = 1;
+    for (int i = 1; i < len; i++)
+    {
+        if (s[i] == 'V' && s[i + 1] == 'K')
+        {
+            cnt++;
+        }
+        if (flag && (s[i] == 'V' && s[i + 1] == 'V' && s[i + 2] != 'K' || s[i - 1] != 'V' && s[i] == 'K' && s[i + 1] == 'K'))
+        {
+            cnt++;
+            flag = 0;
+        }
+    }
+    cout << cnt;
     return 0;
 }
